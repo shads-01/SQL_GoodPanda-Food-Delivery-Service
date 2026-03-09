b68e8bf77af756203e8292ec5dc3cb0be5e90ec6
Shahadat Hasan <tamimshahadat15@gmail.com>
2026-03-09 16:39:03 +0600

    fix: use $_ENV instead of app()->environment() in bootstrap for CSRF testing exception

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        if (app()->environment('testing')) {
+        if (($_ENV['APP_ENV'] ?? null) === 'testing') {
             $middleware->validateCsrfTokens(except: ['*']);
         }
     })
